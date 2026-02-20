1d3bdce19373e0335ff9a2dd5b80f5fd1db8f495
rishikrishna1708-tech <rishikrishna1708@gmail.com>
2026-02-20 16:07:55 +0530

    Create add_two_integers.c

---
+++ C/add_two_integers.c
@@ -0,0 +1,17 @@
+/*
+Problem: Add Two Integers
+
+Approach:
+Return the sum of two integers using the + operator.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+
+Language: C
+*/
+
+int sum(int a, int b) {
+    return a+b;
+}
+
+
