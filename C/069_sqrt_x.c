fe508fb9103139d7b8d1743f33e60c4fee44da90
rishikrishna1708-tech <rishikrishna1708@gmail.com>
2026-02-20 12:44:16 +0530

    Create 069_sqrt_x.c

---
+++ C/069_sqrt_x.c
@@ -0,0 +1,18 @@
+/*
+Problem: 69. Sqrt(x)
+
+Approach:
+Used built-in sqrt() function from math.h to compute square root.
+Returned integer part of the result.
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+
+Language: C
+*/
+
+#include <math.h>
+
+int mySqrt(int x) {
+    return sqrt(x);
+}
