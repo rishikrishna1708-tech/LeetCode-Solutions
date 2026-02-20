b86df9eda4f1d7875a7d5c3849118a18b8ebc786
rishikrishna1708-tech <rishikrishna1708@gmail.com>
2026-02-20 20:26:35 +0530

    Update 1984_minimum_difference_between_k_scores.c

--- C/1984_minimum_difference_between_k_scores.c
+++ C/1984_minimum_difference_between_k_scores.c
@@ -2,8 +2,8 @@
 Problem: 1984. Minimum Difference Between Highest and Lowest of K Scores
 
 Approach:
-Sort array in descending order and compute difference between
-elements of each window of size k.
+Sort array in ascending order and use sliding window of size k
+to compute minimum difference.
 
 Time Complexity: O(n^2)
 Space Complexity: O(1)
@@ -12,25 +12,23 @@ Language: C
 */
 
 int minimumDifference(int* nums, int numsSize, int k) {
-    if(numsSize<=1)return 0;
+    if (k<=1) return 0;
     int temp;
-    for(int i=0;i<numsSize;i++)
-    {
-        for(int j=i+1;j<numsSize;j++)
-        {
-            if(nums[i]<nums[j]){
+    for (int i=0;i<numsSize;i++) {
+        for (int j=i+1;j<numsSize;j++) {
+            if (nums[i]>nums[j]) {
                 temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
             }
         }
     }
-    int minD=nums[0]-nums[k-1];
+    int minD=nums[k-1]-nums[0];
 
-    for(int i=1;i<=numsSize-k;i++){
-        int diff=nums[i]-nums[i+k-1];
-        if(diff<minD)
+    for (int i=1;i<=numsSize-k;i++) {
+        int diff=nums[i+k-1]-nums[i];
+        if (diff<minD)
             minD=diff;
     }
-    return minD;  
+    return minD;
 }
